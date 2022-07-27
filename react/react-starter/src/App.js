861007d4611ac03634ad515c4e31d024223e3134
hellonayeon <hellonykwon@gmail.com>
2022-07-27 10:27:00 +0900

    태그 사용

--- react/react-starter/src/App.js
+++ react/react-starter/src/App.js
@@ -2,26 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function HelloReact() {
-  return 
+  return <div>안녕 리액트</div>
 }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HelloReact></HelloReact>
     </div>
   );
 }
